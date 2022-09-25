bc5a7b24bc902f391c4a8851030a18585b9bbea0
Allan Jardine <allan@datatables.net>
2022-09-25 15:47:11 +0000

    Sync DistRepos

--- js/buttons.bootstrap5.js
+++ js/buttons.bootstrap5.js
@@ -1,3 +1,4 @@
+
 /*! Bootstrap integration for DataTables' Buttons
  * ©2016 SpryMedia Ltd - datatables.net/license
  */
@@ -13,16 +14,25 @@
 		// CommonJS
 		module.exports = function (root, $) {
 			if ( ! root ) {
+				// CommonJS environments without a window global must pass a
+				// root. This will give an error otherwise
 				root = window;
 			}
 
-			if ( ! $ || ! $.fn.dataTable ) {
-				$ = require('datatables.net-bs5')(root, $).$;
+			if ( ! $ ) {
+				$ = typeof window !== 'undefined' ? // jQuery's factory checks for a global window
+					require('jquery') :
+					require('jquery')( root );
 			}
 
-			if ( ! $.fn.dataTable.Buttons ) {
+			if ( ! $.fn.dataTable ) {
+				require('datatables.net-bs5')(root, $);
+			}
+
+			if ( ! $.fn.dataTable ) {
 				require('datatables.net-buttons')(root, $);
 			}
+
 
 			return factory( $, root, root.document );
 		};
@@ -34,6 +44,8 @@
 }(function( $, window, document, undefined ) {
 'use strict';
 var DataTable = $.fn.dataTable;
+
+
 
 $.extend( true, DataTable.Buttons.defaults, {
 	dom: {
@@ -83,5 +95,6 @@ $.extend( true, DataTable.Buttons.defaults, {
 DataTable.ext.buttons.collection.className += ' dropdown-toggle';
 DataTable.ext.buttons.collection.rightAlignClassName = 'dropdown-menu-right';
 
-return DataTable.Buttons;
+
+return DataTable;
 }));
