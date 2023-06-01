66eeed39c6d2e069832ed0f39a5d4b604dc211c3
Allan Jardine <allan.jardine@sprymedia.co.uk>
2023-06-01 16:46:12 +0100

    b5808f9bcbbde4eb10f57606f294a912c3fc463c Dev: Reformatting with PrettierMore
    ed4cd823f52405918e2bfe4b534a4c5bbcb0d87d Dev: Tidy some examples where dev work as creapt in
    
    Sync to source repo @ed4cd823f52405918e2bfe4b534a4c5bbcb0d87d

--- js/buttons.bootstrap5.js
+++ js/buttons.bootstrap5.js
@@ -1,5 +1,5 @@
 /*! Bootstrap integration for DataTables' Buttons
- * ©2016 SpryMedia Ltd - datatables.net/license
+ * © SpryMedia Ltd - datatables.net/license
  */
 
 (function( factory ){
@@ -53,7 +53,7 @@ var DataTable = $.fn.dataTable;
 
 
 
-$.extend( true, DataTable.Buttons.defaults, {
+$.extend(true, DataTable.Buttons.defaults, {
 	dom: {
 		container: {
 			className: 'dt-buttons btn-group flex-wrap'
@@ -91,7 +91,8 @@ $.extend( true, DataTable.Buttons.defaults, {
 			dropdown: {
 				tag: 'button',
 				dropHtml: '',
-				className: 'btn btn-secondary dt-button-split-drop dropdown-toggle dropdown-toggle-split',
+				className:
+					'btn btn-secondary dt-button-split-drop dropdown-toggle dropdown-toggle-split',
 				closeButton: false,
 				align: 'split-left',
 				splitAlignClass: 'dt-button-split-left'
@@ -99,16 +100,14 @@ $.extend( true, DataTable.Buttons.defaults, {
 			wrapper: {
 				tag: 'div',
 				className: 'dt-button-split btn-group',
-				closeButton: false,
+				closeButton: false
 			}
 		}
 	},
-	buttonCreated: function ( config, button ) {
-		return config.buttons ?
-			$('<div class="btn-group"/>').append(button) :
-			button;
+	buttonCreated: function (config, button) {
+		return config.buttons ? $('<div class="btn-group"/>').append(button) : button;
 	}
-} );
+});
 
 DataTable.ext.buttons.collection.className += ' dropdown-toggle';
 DataTable.ext.buttons.collection.rightAlignClassName = 'dropdown-menu-right';
