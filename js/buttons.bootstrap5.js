6e214bbe17f2c3ff4a1f8d8f07f2bd022e213620
Allan Jardine <allan@datatables.net>
2026-05-07 15:34:32 +0000

    Updating package for DataTables 3

--- js/buttons.bootstrap5.js
+++ js/buttons.bootstrap5.js
@@ -1,8 +1,56 @@
-/*! Bootstrap integration for DataTables' Buttons
- * © SpryMedia Ltd - datatables.net/license
+/*! Buttons Bootstrap 5 styling 4.0.0-beta.1 for DataTables
+ * Copyright (c) SpryMedia Ltd - datatables.net/license
  */
 
-$.extend(true, DataTable.Buttons.defaults, {
+(function(factory){
+	if (typeof define === 'function' && define.amd) {
+		// AMD
+		define(['datatables.net-bs5', 'datatables.net-buttons'], function (dt) {
+			return factory(window, document, dt);
+		});
+	}
+	else if (typeof exports === 'object') {
+		// CommonJS
+		var cjsRequires = function (root) {
+			if (! root.DataTable) {
+				require('datatables.net-bs5')(root);
+			}
+
+			if (! window.DataTable.Buttons) {
+				require('datatables.net-buttons')(root);
+			}
+		};
+
+		if (typeof window === 'undefined') {
+			module.exports = function (root) {
+				if (! root) {
+					// CommonJS environments without a window global must pass a
+					// root. This will give an error otherwise
+					root = window;
+				}
+
+				cjsRequires(root);
+				return factory(root, root.document, root.DataTable);
+			};
+		}
+		else {
+			cjsRequires(window);
+			module.exports = factory(window, window.document, window.DataTable);
+		}
+	}
+	else {
+		// Browser
+		factory(window, document, window.DataTable);
+	}
+}(function(window, document, DataTable) {
+'use strict';
+
+
+
+var Dom = DataTable.Dom;
+var util = DataTable.util;
+
+util.object.assignDeep(DataTable.Buttons.defaults, {
 	dom: {
 		container: {
 			className: 'dt-buttons btn-group flex-wrap'
@@ -61,9 +109,13 @@ $.extend(true, DataTable.Buttons.defaults, {
 	},
 	buttonCreated: function (config, button, inCollection) {
 		return config.buttons && ! inCollection
-			? $('<div class="btn-group"/>').append(button)
+			? Dom.c('div').classAdd('btn-group a').append(button)
 			: button;
 	}
 });
 
 DataTable.ext.buttons.collection.rightAlignClassName = 'dropdown-menu-right';
+
+
+return DataTable;
+}));
